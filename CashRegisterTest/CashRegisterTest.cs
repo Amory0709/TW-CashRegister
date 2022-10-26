ad7d8b1eaa907d8c1f993ce7a6015c37579a3868
Mengyu <mhan8@slb.com>
2022-10-26 16:07:50 +0800

    test: q3 and q4. using moq spy and stub

--- CashRegisterTest/CashRegisterTest.cs
+++ CashRegisterTest/CashRegisterTest.cs
@@ -1,7 +1,8 @@
 namespace CashRegisterTest
 {
 	using CashRegister;
-	using Xunit;
+    using Moq;
+    using Xunit;
 
 	public class CashRegisterTest
 	{
@@ -17,6 +18,35 @@ namespace CashRegisterTest
 			//then
 			//verify that cashRegister.process will trigger print
 			Assert.True(printer.HasPrinted);
+		}
+
+		[Fact]
+		public void Should_print_when_using_spy_printer()
+        {
+			//given
+			var spyPrinter = new Mock<Printer>();
+			var cashRegister = new CashRegister(spyPrinter.Object);
+			var purchase = new Purchase();
+			//when
+			cashRegister.Process(purchase);
+			//then
+			//verify that cashRegister.process will trigger print
+			spyPrinter.Verify(_ => _.Print(It.IsAny<string>()));
+		}
+
+		[Fact]
+		public void Should_print_when_using_stub_purchase()
+        {
+			//given
+			var spyPrinter = new Mock<Printer>();
+			var cashRegister = new CashRegister(spyPrinter.Object);
+			var mockPurchase = new Mock<Purchase>();
+			mockPurchase.Setup(_ => _.AsString()).Returns("stub content");
+			//when
+			cashRegister.Process(mockPurchase.Object);
+			//then
+			//verify that cashRegister.process will trigger print
+			spyPrinter.Verify(_ => _.Print("stub content"));
 		}
 	}
 }
